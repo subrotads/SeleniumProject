23908c17292dbde69877c7b32b2a47f9e92829bb
subrotads <subrotads@gmail.com>
2026-01-19 12:30:21 +0000

    push

--- src/test/java/Project/Datap.java
+++ src/test/java/Project/Datap.java
@@ -2,7 +2,6 @@ package Project;
 
 import org.testng.annotations.Test;
 
-//hello check
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
